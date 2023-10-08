93d6a1770c23a2bc827ef1ed04a39ccb69c17c40
Jakub Miętus <hidmir305@gmail.com>
2023-10-08 10:59:01 +0200

    Create GameManager class and prefab.

---
+++ VoidChase/Assets/Scripts/GameManagement/GameManager.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using VoidChase.Utilities;
+
+namespace VoidChase.GameManagement
+{
+	public class GameManager : SingletonMonoBehaviour<GameManager>
+	{
+		[field: Header(InspectorNames.REFERENCES_NAME)]
+		[field: SerializeField]
+		public SceneBoundariesController CurrentSceneBoundariesController { get; private set; }
+	}
+}
