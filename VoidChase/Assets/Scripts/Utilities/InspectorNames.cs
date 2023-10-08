4620ac06917953d3ad09b10a1c44a039ac4613ff
Jakub Miętus <hidmir305@gmail.com>
2023-10-08 10:39:29 +0200

    Create InspectorNames class.

---
+++ VoidChase/Assets/Scripts/Utilities/InspectorNames.cs
@@ -0,0 +1,9 @@
+namespace VoidChase.Utilities
+{
+	public static class InspectorNames
+	{
+		public const string SETTINGS_NAME = "Settings";
+		public const string REFERENCES_NAME = "References";
+		public const string VISUALIZATION_NAME = "Visualization";
+	}
+}
