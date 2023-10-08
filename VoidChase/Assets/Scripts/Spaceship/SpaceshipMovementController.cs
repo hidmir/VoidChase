95a9e40ca146fd47aff67e8fea0b21c4a1215c68
Jakub Miętus <hidmir305@gmail.com>
2023-10-08 11:20:17 +0200

    Use SceneBoundariesController in spaceship movement.

--- VoidChase/Assets/Scripts/Spaceship/SpaceshipMovementController.cs
+++ VoidChase/Assets/Scripts/Spaceship/SpaceshipMovementController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using VoidChase.GameManagement;
 using VoidChase.Spaceship.Input;
 using VoidChase.Utilities;
 
@@ -26,7 +27,7 @@ namespace VoidChase.Spaceship
 			Vector2 movementInputValue = SpaceshipInputProvider.Instance.MovementInputAction.ReadValue<Vector2>();
 			Vector2 velocity = movementInputValue != Vector2.zero ? GetMovementVelocity(movementInputValue) : GetBrakingVelocity();
 
-			CurrentRigidbody.velocity = velocity;
+			CurrentRigidbody.velocity = ClampVelocityToSceneBoundaries(velocity);
 		}
 
 		private Vector2 GetMovementVelocity (Vector2 movementInputValue)
@@ -37,6 +38,22 @@ namespace VoidChase.Spaceship
 		private Vector2 GetBrakingVelocity ()
 		{
 			return Vector2.Lerp(CurrentRigidbody.velocity, Vector2.zero, BrakingSpeed * Time.deltaTime);
+		}
+
+		private Vector2 ClampVelocityToSceneBoundaries (Vector2 velocity)
+		{
+			(float maxX, float minX, float maxY, float minY) = GameManager.Instance.CurrentSceneBoundariesController.GetMaxMinPositions();
+			Vector3 currentPosition = CurrentRigidbody.transform.position;
+
+			bool isMaxXReached = currentPosition.x >= maxX;
+			bool isMinXReached = currentPosition.x <= minX;
+			bool isMaxYReached = currentPosition.y >= maxY;
+			bool isMinYReached = currentPosition.y <= minY;
+
+			float clampedVelocityOnX = (isMaxXReached && velocity.x > 0) || (isMinXReached && velocity.x < 0) ? 0.0f : velocity.x;
+			float clampedVelocityOnY = (isMaxYReached && velocity.y > 0) || (isMinYReached && velocity.y < 0) ? 0.0f : velocity.y;
+
+			return new Vector2(clampedVelocityOnX, clampedVelocityOnY);
 		}
 	}
 }
