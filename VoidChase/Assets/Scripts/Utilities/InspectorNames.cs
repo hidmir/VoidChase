5d9c2dbd2265541bbd1d17e8148366c9568fcf55
Jakub Miętus <hidmir305@gmail.com>
2023-10-08 10:50:00 +0200

    Add EVENTS_NAME const to InspectorNames class.

--- VoidChase/Assets/Scripts/Utilities/InspectorNames.cs
+++ VoidChase/Assets/Scripts/Utilities/InspectorNames.cs
@@ -4,6 +4,7 @@ namespace VoidChase.Utilities
 	{
 		public const string SETTINGS_NAME = "Settings";
 		public const string REFERENCES_NAME = "References";
+		public const string EVENTS_NAME = "Events";
 		public const string VISUALIZATION_NAME = "Visualization";
 	}
 }
