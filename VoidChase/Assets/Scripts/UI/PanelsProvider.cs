f7992618852aac997eb274fed950d2911babdc9b
Jakub Miętus <hidmir305@gmail.com>
2023-11-04 16:10:39 +0100

    Create PanelsProvider and UIManager scripts.

---
+++ VoidChase/Assets/Scripts/UI/PanelsProvider.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+using VoidChase.Utilities;
+
+namespace VoidChase.UI
+{
+	public class PanelsProvider : ObjectsProvider<PanelsProvider, PanelData, GameObject> { }
+}
---
+++ VoidChase/Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using VoidChase.Utilities;
+
+namespace VoidChase.UI
+{
+	public class UIManager : SingletonMonoBehaviour<UIManager>
+	{
+		[field: SerializeField]
+		private PanelsProvider CurrentPanelsProvider { get; set; }
+
+		public void ShowPanel (string panelName)
+		{
+			SetPanelState(panelName, true);
+		}
+
+		public void HidePanel (string panelName)
+		{
+			SetPanelState(panelName, false);
+		}
+
+		protected virtual void Start ()
+		{
+			SetAllPanelsState(false);
+		}
+
+		private void SetAllPanelsState (bool isEnabled)
+		{
+			foreach (GameObject panel in CurrentPanelsProvider.GetAllObjects())
+			{
+				panel.SetActive(isEnabled);
+			}
+		}
+
+		private void SetPanelState (string panelName, bool isEnabled)
+		{
+			if (CurrentPanelsProvider.TryGetObject(panelName, out GameObject objectReference))
+			{
+				objectReference.SetActive(isEnabled);
+			}
+		}
+	}
+}
