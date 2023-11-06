740116bbb8120e9b220da6a9a10648e0b700e84e
Jakub Miętus <hidmir305@gmail.com>
2023-11-06 20:32:28 +0100

    Add Content property to View and create SetContentState method.

--- VoidChase/Assets/Scripts/UI/MVC/View.cs
+++ VoidChase/Assets/Scripts/UI/MVC/View.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 namespace VoidChase.UI
 {
-	public class View : MonoBehaviour { }
+	public class View : MonoBehaviour
+	{
+		[field: SerializeField]
+		protected GameObject Content { get; private set; }
+
+		public void SetContentState (bool isEnabled)
+		{
+			Content.SetActive(isEnabled);
+		}
+	}
 }
