909199882ff5b232d002ab28f1b782a01f3f049b
Jakub Miętus <hidmir305@gmail.com>
2023-11-07 21:47:30 +0100

    Create IsGamePaused global shared variable.

---
+++ VoidChase/Assets/Scripts/GameManagement/GameGlobalVariables.cs
@@ -0,0 +1,9 @@
+using VoidChase.Utilities;
+
+namespace VoidChase.GameManagement
+{
+	public static class GameGlobalVariables
+	{
+		public static CodeSharedVariable<bool> IsGamePaused = new ();
+	}
+}
