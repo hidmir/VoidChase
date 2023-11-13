4406671dfa0ffa645932526e0bc6b74db7a20196
Jakub Miętus <hidmir305@gmail.com>
2023-11-13 20:57:53 +0100

    Create StringCollectionNames class.

---
+++ VoidChase/Assets/Scripts/Utilities/StringCollectionNames.cs
@@ -0,0 +1,10 @@
+namespace VoidChase.Utilities
+{
+	public static class StringCollectionNames
+	{
+		public const string PANELS_COLLECTION_NAME = "PanelsNames";
+		public const string WEAPONS_COLLECTION_NAME = "WeaponsNames";
+		public const string ASTEROIDS_COLLECTION_NAME = "AsteroidsNames";
+		public const string SPACESHIP_PROJECTILES_COLLECTION_NAME = "SpaceshipProjectilesNames";
+	}
+}
