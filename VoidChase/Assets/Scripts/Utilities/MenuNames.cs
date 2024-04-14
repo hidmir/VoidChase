190f1757c2bf0ab4f9c6a2651ae9d76732f26424
Jakub Miętus <hidmir305@gmail.com>
2024-04-14 18:57:23 +0200

    Create MenuNames class.

---
+++ VoidChase/Assets/Scripts/Utilities/MenuNames.cs
@@ -0,0 +1,9 @@
+namespace VoidChase.Utilities
+{
+	public static class MenuNames
+	{
+		public const string MENU_ROOT = "VoidChase/";
+		public const string SCRIPTABLE_OBJECTS_PATH = MENU_ROOT + "ScriptableObjects/";
+		public const string SCENE_MANAGEMENT_PATH = MENU_ROOT + "SceneManagement/";
+	}
+}
