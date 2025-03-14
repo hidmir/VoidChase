06ebc07faccdaa0b7ae24eff6fd574bc2bd9c7c2
Jakub Miętus <hidmir305@gmail.com>
2025-03-14 14:47:57 +0100

    Improve StartInitSceneToolbarButton class.

--- VoidChase/Assets/Scripts/Utilities/Editor/ToolbarExtenders/StartInitSceneToolbarButton.cs
+++ VoidChase/Assets/Scripts/Utilities/Editor/ToolbarExtenders/StartInitSceneToolbarButton.cs
@@ -9,9 +9,18 @@ namespace VoidChase.Utilities.ToolbarExtenders
 	[InitializeOnLoad]
 	public static class StartInitSceneToolbarButton
 	{
+		private const string PreviousSceneKey = "PreviousScenePath";
+
+		private static string PreviousScene
+		{
+			get => EditorPrefs.GetString(PreviousSceneKey, "");
+			set => EditorPrefs.SetString(PreviousSceneKey, value);
+		}
+
 		static StartInitSceneToolbarButton ()
 		{
 			ToolbarExtender.RightToolbarGUI.Add(OnToolbarGUI);
+			EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
 		}
 
 		private static void OnToolbarGUI ()
@@ -24,16 +33,33 @@ namespace VoidChase.Utilities.ToolbarExtenders
 
 		private static void StartGameScene ()
 		{
-			string scenePath = SceneUtility.GetScenePathByBuildIndex(0);
+			PreviousScene = SceneManager.GetActiveScene().path;
+			string initScenePath = SceneUtility.GetScenePathByBuildIndex(0);
 
-			if (string.IsNullOrEmpty(scenePath))
+			if (string.IsNullOrEmpty(initScenePath))
 			{
 				Debug.LogError("Scene with index 0 has not been added to Build Settings!");
 				return;
 			}
 
-			EditorSceneManager.OpenScene(scenePath);
+			EditorSceneManager.OpenScene(initScenePath);
 			EditorApplication.isPlaying = true;
+		}
+
+		private static void OnPlayModeStateChanged (PlayModeStateChange state)
+		{
+			if (state == PlayModeStateChange.EnteredEditMode)
+			{
+				string previousScenePath = PreviousScene;
+
+				if (string.IsNullOrEmpty(previousScenePath))
+				{
+					return;
+				}
+
+				EditorSceneManager.OpenScene(previousScenePath);
+				PreviousScene = null;
+			}
 		}
 	}
 }
