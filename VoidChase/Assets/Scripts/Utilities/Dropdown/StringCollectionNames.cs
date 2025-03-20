bbd1821dc5b364351af79916451e2ae6fd40aa07
Jakub Miętus <hidmir305@gmail.com>
2025-03-20 18:47:58 +0100

    Add LEVELS_COLLECTION_NAME to StringCollectionNames.

--- VoidChase/Assets/Scripts/Utilities/Dropdown/StringCollectionNames.cs
+++ VoidChase/Assets/Scripts/Utilities/Dropdown/StringCollectionNames.cs
@@ -4,5 +4,6 @@ namespace VoidChase.Utilities.Dropdown
 	{
 		public const string PANELS_COLLECTION_NAME = "PanelsNames";
 		public const string WEAPONS_COLLECTION_NAME = "WeaponsNames";
+		public const string LEVELS_COLLECTION_NAME = "LevelsNames";
 	}
 }
